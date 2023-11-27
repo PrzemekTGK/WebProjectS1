52eacb669a4af653fc6574688a983e87734693ce
PrzemekTGK <PrzemekTGK@gmail.com>
2023-11-27 01:20:57 +0000

    minor updates

---
+++ JS/loadPage.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    loadPage('/Pages/home.html'); // Change the URL to your default subpage
+});
+
+function loadPage(url) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'html',
+        success: function (html) {
+            $('#contentDiv').html(html);
+        },
+        error: function (error) {
+            console.error(`Failed to fetch: ${url}`, error);
+        }
+    });
+}
