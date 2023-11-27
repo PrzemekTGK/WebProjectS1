477fd26902f1dade8caaaf57ca817c5d9e40a11b
PrzemekTGK <PrzemekTGK@gmail.com>
2023-11-27 17:22:39 +0000

    Refresh fix update

--- JS/loadPage.js
+++ JS/loadPage.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-    loadPage('/Pages/home.html');
+    var lastLoadedPage = localStorage.getItem('lastLoadedPage');
+
+    if (lastLoadedPage) {
+        loadPage(lastLoadedPage);
+    } else {
+        loadPage('/Pages/home.html');
+    }
 });
 
 function loadPage(url, carIndex, callback) {
-    
     $.getScript("../Js/carDetails.js", function () {
-
         $.ajax({
             url: url,
             type: 'GET',
             dataType: 'html',
             success: function (html) {
-                $('#contentDiv').html(html);                
+                $('#contentDiv').html(html);
                 updateCarDetails(carIndex);
+                localStorage.setItem('lastLoadedPage', url);
             },
             error: function (error) {
                 console.error(`Failed to fetch: ${url}`, error);
