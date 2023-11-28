57a50fc3e130f74af9b6a7cc004a70fd9e79a150
PrzemekTGK <PrzemekTGK@gmail.com>
2023-11-28 22:04:57 +0000

    nav link highlight final fix

--- JS/currentPage.js
+++ JS/currentPage.js
@@ -1,19 +1,21 @@
 function highlightCurrentPage() {
-    // Get the current page name
     var currentPage = window.location.pathname.split('/').pop();
 
-    // Find the corresponding link in the navigation bar
     var navLinks = $('.navbar-container').find('.nav-link');
     navLinks.each(function () {
         var linkPage = $(this).attr('href').split('/').pop();
         if (linkPage === currentPage) {
-            // Add the "current" class to the corresponding link
             $(this).addClass('current');
         }
     });
 }
 
-// Highlight the current page when the document is ready
 $(function () {
-    highlightCurrentPage();
+    if (window.location.pathname.endsWith('index.html')) {
+        setTimeout(function () {
+            highlightCurrentPage();
+        }, 500);
+    } else {
+        highlightCurrentPage();
+    }
 });
