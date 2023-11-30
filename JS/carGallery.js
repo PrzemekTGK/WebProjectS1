c787ff92d320cb8804383cb0b88532a6713965cc
PrzemekTGK <PrzemekTGK@gmail.com>
2023-11-30 05:01:25 +0000

    Initial finance car selection update

--- JS/carGallery.js
+++ JS/carGallery.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-    // Fetch car data from JSON file
-    $.getJSON("../Json/cars.json", function(cars) {
-      // Reference to the cars container
-      var carsContainer = $("#carsContainer");
-  
-      // Iterate through each car in the JSON data
-      cars.forEach(function(car, index) {
-        // Create HTML for each card
-        var cardHtml = `
+$(document).ready(function () {
+  // Fetch car data from JSON file
+  $.getJSON("../Json/cars.json", function (cars) {
+    // Reference to the cars container
+    var carsContainer = $("#carsContainer");
+
+    // Iterate through each car in the JSON data
+    cars.forEach(function (car, index) {
+      // Create HTML for each card
+      var cardHtml = `
           <div class="col-sm-12 col-md-4 col-lg-4 car-col">
             <div class="card" style="width: 18rem;">
               <img class="card-img-top" src="/Images/Cards/${car.image}" alt="Card image cap">
@@ -20,15 +20,15 @@ $(document).ready(function() {
             </div>
           </div>
         `;
-  
-        // Check if it's the first card in a new row
-        if (index % 3 === 0) {
-          // Create a new row div
-          carsContainer.append('<div class="row car-row"></div>');
-        }
-  
-        // Append the card to the current row
-        carsContainer.find('.row.car-row:last').append(cardHtml);
-      });
+
+      // Check if it's the first card in a new row
+      if (index % 3 === 0) {
+        // Create a new row div
+        carsContainer.append('<div class="row car-row"></div>');
+      }
+
+      // Append the card to the current row
+      carsContainer.find('.row.car-row:last').append(cardHtml);
     });
   });
+});
--- JS/carGalleryFinance.js
+++ JS/carGalleryFinance.js
@@ -1,33 +1,36 @@
-$(document).ready(function() {
-    // Fetch car data from JSON file
-    $.getJSON("../Json/cars.json", function(cars) {
-      // Reference to the cars container
-      var carsContainer = $("#carsContainer");
-  
-      // Iterate through each car in the JSON data
-      cars.forEach(function(car, index) {
-        // Create HTML for each card
-        var cardHtml = `
-          <div class="col-sm-12 col-md-4 col-lg-4 car-col">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="/Images/Cards/${car.image}" alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">${car.make} ${car.model}</h5>
-                <p class="card-text">Year: ${car.year} 
-                <br>Price: €${car.price}</p>
-              </div>
+$(document).ready(function () {
+  // Fetch car data from JSON file
+  $.getJSON("../Json/cars.json", function (cars) {
+    // Reference to the cars container
+    var carsContainer = $("#financeCarsContainer");
+
+    // Iterate through each car in the JSON data
+    cars.forEach(function (car, index) {
+      // Create HTML for each card
+      var cardHtml = `
+          <div class="col car-col-finance">
+            <div class="finance-card mt-4">
+              <a href="#" style="text-decoration: none;">
+                <img class="card-img-top" src="/Images/Cards/${car.image}" alt="Card image cap">
+                <div class="card-body p-2" style="color: rgb(255, 255, 255);">
+                  <p class="card-title" style="color: orange; font-weight: bold; font-size: large; text-wrap: wrap;">${car.make} ${car.model}<p>
+                  <p class="card-text">Year: ${car.year}
+                    <br>Price: €${car.price}
+                  </p>
+                </div>
+              </a>
             </div>
           </div>
         `;
-  
-        // Check if it's the first card in a new row
-        if (index % 2 === 0) {
-          // Create a new row div
-          carsContainer.append('<div class="row car-row"></div>');
-        }
-  
-        // Append the card to the current row
-        carsContainer.find('.row.car-row:last').append(cardHtml);
-      });
+
+      // Check if it's the first card in a new row
+      if (index % 2 === 0) {
+        // Create a new row div
+        carsContainer.append('<div class="row car-row-finance"></div>');
+      }
+
+      // Append the card to the current row
+      carsContainer.find('.row.car-row-finance:last').append(cardHtml);
     });
   });
+});
