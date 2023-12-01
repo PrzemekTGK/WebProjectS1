0863ab0d49a2e0b6ce0f2b4fb8650f59c6911d15
PrzemekTGK <PrzemekTGK@gmail.com>
2023-12-01 04:44:28 +0000

    search bar functionality update

---
+++ JS/searchButton.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the search button and input field
+    var searchBtn = document.getElementById("searchBtn");
+    var searchInput = document.getElementById("searchInput");
+
+    // Add click event listener to the search button
+    searchBtn.addEventListener("click", function () {
+        console.log("Search Button clicked! ");
+        // Get the user's input from the search input field
+        var userInput = searchInput.value;
+
+        // Construct the URL for the search page with the user's input
+        var searchUrl = "../Pages/search.html?query=" + encodeURIComponent(userInput);
+
+        // Redirect the user to the search page
+        window.location.href = searchUrl;
+    });
+});
---
+++ JS/searchResult.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+  var urlParams = new URLSearchParams(window.location.search);
+  var searchTerm = urlParams.get('query');
+
+  console.log("Search term from URL:", searchTerm);
+
+  $.getJSON("../Json/cars.json", function (cars) {
+    console.log("Cars data:", cars);
+
+    var searchContainer = $("#searchContainer");
+    var searchInput = $("#searchInput");
+
+    searchInput.val(searchTerm);
+
+    function updateSearchResults() {
+
+      searchContainer.empty();      
+
+      var filteredCars = cars.filter(function (car) {
+        var lowerCaseSearchTerm = searchTerm.toLowerCase();
+
+        return Object.values(car).some(function (value) {
+          if (typeof value === 'string') {
+            return value.toLowerCase().includes(lowerCaseSearchTerm);
+          }
+
+          return false;
+        });
+      });
+
+      console.log("Filtered cars:", filteredCars);
+
+      if (searchTerm === "") {
+        searchContainer.append("<div class=\"container\" id=\"noResultMessageContainer\"><h1>No search term entered.</h1></div>");
+      } else if (filteredCars.length === 0) {
+        searchContainer.append("<div class=\"container\" id=\"noResultMessageContainer\"><h1>No matching cars found.</h1></div>");
+      } else {
+        filteredCars.forEach(function (car, index) {
+          var cardHtml = `
+              <div class="col-sm-12 col-md-4 col-lg-4 car-col">
+                <div class="card" style="width: 18rem;">
+                  <img class="card-img-top" src="/Images/Cards/${car.image}" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">${car.make} ${car.model}</h5>
+                    <p class="card-text">Year: ${car.year} 
+                    <br>Price: €${car.price}</p>
+                    <a href="/Pages/car.html?carIndex=${car.index}" class="btn car-link">More Details</a>
+                  </div>
+                </div>
+              </div>
+            `;
+
+          if (index % 3 === 0) {
+            searchContainer.append('<div class="row car-row"></div>');
+          }
+
+          searchContainer.find('.row.car-row:last').append(cardHtml);
+        });
+      }
+    }
+
+    searchInput.on("input", function () {
+      searchTerm = $(this).val().toLowerCase();
+      console.log("Updated search term:", searchTerm);
+      updateSearchResults();
+    });
+
+    updateSearchResults();
+  });
+});
