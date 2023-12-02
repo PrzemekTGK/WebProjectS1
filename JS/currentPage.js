bb13cc109250e703b1800051f6058c0e8a43fa30
PrzemekTGK <PrzemekTGK@gmail.com>
2023-12-02 17:11:29 +0000

    currentPage.js minor update

--- JS/currentPage.js
+++ JS/currentPage.js
@@ -15,7 +15,7 @@ function highlightCurrentPage() {
         } else if (currentPage === 'car.html') {
             $('.navbar-container #navLinkCars').addClass('current');
         } else if (currentPage === 'finance.html') {
-            $('.navbar-container #navLinkFinance').addClass('current');
+            $('.navbar-container #navLinkFinance').addClass('current');            
             updateCarDetails(carIndex);
         } 
     });
@@ -40,19 +40,21 @@ function updateCarDetails(carIndex) {
         success: function (data) {
             const selectedCar = data[carIndex];
             const template = $('#carDetailsTemplate').html();
-
-            const filledTemplate = template
+            
+            try { 
+                const filledTemplate = template
                 .replace(/{MAKE}/g, selectedCar.make)
                 .replace(/{MODEL}/g, selectedCar.model)
                 .replace(/{YEAR}/g, selectedCar.year)
                 .replace(/{ENGINE_SIZE}/g, selectedCar.engine_size)
-                .replace(/{DESCRIPTION}/g, selectedCar.description)
                 .replace(/{FUEL_TYPE}/g, selectedCar.fuel_type)
                 .replace(/{TRANSMISSION}/g, selectedCar.transmission)
                 .replace(/{PRICE}/g, selectedCar.price)
                 .replace(/{INDEX}/g, selectedCar.index);
-            $('#carDetailsContainer').html(filledTemplate);
-            $('#carImage').attr('src', '../Images/Cars/' + selectedCar.image);
+                $('#carDetailsContainer').html(filledTemplate);
+                $('#carImage').attr('src', '../Images/Cars/' + selectedCar.image);
+            } catch (error) {
+            }           
 
             var carFullPriceInput = document.getElementById("carFullPrice");
             if(carFullPriceInput){
