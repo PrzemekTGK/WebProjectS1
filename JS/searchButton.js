c43815543a0b278f94b264194b7874331f9a3044
PrzemekTGK <PrzemekTGK@gmail.com>
2023-12-02 05:51:49 +0000

    searchbar fix update

--- JS/searchButton.js
+++ JS/searchButton.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
     console.log("Adding Search Button Event listener");
-    // Get references to the search button and input field
-    var searchBtn = document.getElementById("searchBtn");
-    var searchInput = document.getElementById("searchInput");
 
-    // Add click event listener to the search button
-    searchBtn.addEventListener("click", function () {
-        console.log("Search Button clicked! ");
-        // Get the user's input from the search input field
-        var userInput = searchInput.value;
+    // Get references to the search button and input field within the navbar
+    setTimeout(function(){
+        var navbar = document.getElementById("navbarHolder"); // Assuming the navbar has an ID "navbarHolder"
+        var searchBtn = navbar.querySelector("#searchBtn"); // Assuming the search button has an ID "searchBtn"
+        var searchInput = navbar.querySelector("#searchInput"); // Assuming the search input has an ID "searchInput"
 
-        // Construct the URL for the search page with the user's input
-        var searchUrl = "../Pages/search.html?query=" + encodeURIComponent(userInput);
-
-        // Redirect the user to the search page
-        window.location.href = searchUrl;
-    });
+        console.log(searchBtn);
+        console.log(searchInput);
+    
+        // Add click event listener to the search button
+        searchBtn.addEventListener("click", function () {
+            console.log("Search Button clicked!");
+    
+            // Get the user's input from the search input field
+            var userInput = searchInput.value;
+    
+            // Construct the URL for the search page with the user's input
+            var searchUrl = "../Pages/search.html?query=" + encodeURIComponent(userInput);
+    
+            // Redirect the user to the search page
+            window.location.href = searchUrl;
+        });
+    }, 200);
 });
